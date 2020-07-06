c7329007d36b0f5b9fde645428c8c9ddef1b4e0d
Snehal Chodankar <snehalchodankar95@gmail.com>
2020-07-06 11:28:11 +0530

    Update AttendenceProject.py

--- AttendenceProject.py
+++ AttendenceProject.py
@@ -78,7 +78,7 @@ while True:
     success, img = capture.read()
     imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
-
+    
     facesCurrentFrame = face_recognition.face_locations(imgSmall)
     encodesCurrentFrame = face_recognition.face_encodings(imgSmall, facesCurrentFrame)
 
@@ -92,16 +92,17 @@ while True:
             name = classNames[matchIndex].upper()
             markAttendence(name)
         else: name = 'UNKNOWN'
-        # print(name)
+
         y1, x2, y2, x1 = faceLocation
         y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-        markAttendence(name)
+        #markAttendence(name)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-        
+
+capture.release()
 cv2.destroyAllWindows()
