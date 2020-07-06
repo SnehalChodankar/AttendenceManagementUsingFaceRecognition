cc8a9ecbd038c163dcf5925000fffb98f50ba1a9
Snehal Chodankar <snehalchodankar95@gmail.com>
2020-07-06 18:51:44 +0530

    Update CaptureMultiImages.py

--- CaptureMultiImages.py
+++ CaptureMultiImages.py
@@ -1,30 +1,30 @@
-import cv2
+import cv2, os
+
+imageDir =  'C:\\Users\\sneha\\Desktop\\AttendenceProject\\database\\Pooja'
 
 cam = cv2.VideoCapture(0)
-
 cv2.namedWindow("test")
 
 img_counter = 0
 
-while True:
+while img_counter<=10:
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
         break
     cv2.imshow("test", frame)
-
+    #label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
     k = cv2.waitKey(1)
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k%256 == 32:
+    else:
         # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
+        img_name = "{}.png".format(img_counter)
+        cv2.imwrite(os.path.join(imageDir, img_name), frame)
         print("{} written!".format(img_name))
         img_counter += 1
 
 cam.release()
-
 cv2.destroyAllWindows()
